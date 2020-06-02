e9690fec39807cf9a22e56287758847b7644e777
leesihoney <siheonl@andrew.cmu.edu>
2020-06-01 21:37:21 -0400

    recap and add switch statement and constants

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ const number = document.querySelector('span');
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // a reducer is a function that modifes your data
 // first it changes your data
 // but also whatever it returns, that is the data of your application
 const countModifier = (count = 0, action) => {
-  if(action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count - 1
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -26,11 +30,11 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
