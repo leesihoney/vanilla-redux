21f3dcf2b4b2fbc9ec703f38b8943a4b54a19661
leesihoney <siheonl@andrew.cmu.edu>
2020-06-01 21:26:37 -0400

    dispatch to provoke reducer

--- src/index.js
+++ src/index.js
@@ -6,8 +6,24 @@ const number = document.querySelector('span');
 // a reducer is a function that modifes your data
 // first it changes your data
 // but also whatever it returns, that is the data of your application
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if(action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+
+console.log(countStore.getState());
