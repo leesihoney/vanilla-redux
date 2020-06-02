07246048e78bc7913e1765def658c4549b687219
leesihoney <siheonl@andrew.cmu.edu>
2020-06-01 21:31:44 -0400

    learned subscribe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
 // a reducer is a function that modifes your data
 // first it changes your data
 // but also whatever it returns, that is the data of your application
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
   if(action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -16,14 +17,20 @@ const countModifier = (count = 0, action) => {
     return count;
   }
 };
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "MINUS"});
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
